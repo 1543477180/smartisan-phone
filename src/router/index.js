36bb4b8727ac57b0148b5da659a5d04167388fa0
lianzhilin <1543477180@qq.com>
2020-07-08 15:24:48 +0800

    上传锤子手机商城项目

--- src/router/index.js
+++ src/router/index.js
@@ -1,27 +1,69 @@
 import Vue from 'vue'
-import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
+import Router from 'vue-router'
+import '@/assets/css/reset.css'
+import '@/assets/css/header.css'
 
-Vue.use(VueRouter)
+import Shop from '@/views/shop'
+import Item from '@/views/item'
+import Cart from '@/views/cart'
+import Checkout from '@/views/checkout'
+import Payment from '@/views/payment'
+import Account from '@/views/account'
+import Order from '@/views/account/order'
+import Address from '@/views/account/address'
 
-const routes = [
-  {
-    path: '/',
-    name: 'Home',
-    component: Home
+Vue.use(Router)
+
+export default new Router({
+  mode: 'history',
+  scrollBehavior (to, from, savePosition) {
+    if (savePosition) {
+      return savePosition
+    } else {
+      return { x: 0, y: 0 }
+    }
   },
-  {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  }
-]
-
-const router = new VueRouter({
-  routes
+  routes: [
+    {
+      path: '/',
+      name: 'Shop',
+      component: Shop
+    },
+    {
+      path: '/item',
+      name: 'Item',
+      component: Item
+    },
+    {
+      path: '/cart',
+      name: 'Cart',
+      component: Cart
+    },
+    {
+      path: '/checkout',
+      name: 'Checkout',
+      component: Checkout
+    },
+    {
+      path: '/payment',
+      name: 'Payment',
+      component: Payment
+    },
+    {
+      path: '/account',
+      component: Account,
+      children: [
+        {
+          path: '',
+          name: 'Account',
+          component: Order
+        },
+        {
+          path: '/address',
+          name: 'Address',
+          component: Address
+        }
+      ]
+    }
+  ]
 })
-
-export default router
